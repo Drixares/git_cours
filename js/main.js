02538804b231c018d972e68b953b8df7d9a149d7
Matteo <Matteo.lorenzoq@gmail.com>
2024-09-18 08:41:52 +0200

    add: switch button + show message error

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const amountInput = document.getElementById('amount');
 const resultatDiv = document.getElementById('resultat'); 
 const url = " https://v6.exchangerate-api.com/v6/39006265cb1ce51335d8eb73/pair" 
 const selects = document.querySelectorAll('select');
+const switchButton = document.getElementById('switch');
 
 const devises = {
   "usd": {
@@ -26,20 +27,27 @@ selects.forEach(select => {
   select.addEventListener('change', () => submitForm());
 })
 
+switchButton.addEventListener('click', () => switchDevises());
+
 // ------- FUNCTIONS ------- //
 async function submitForm() {
   const formData = new FormData(form);
   const { amount, from, to } = Object.fromEntries(formData.entries())
 
   if (Number(amount) === 0 || amount === "") {
+    resultatDiv.textContent = "";
+    document.getElementById("error").textContent = "Please enter a valid amount";
     return;
   }
+
+  document.getElementById("error").textContent = "";
 
   try {
     const response = await fetch(`${url}/${from}/${to}`)
     
     if (!response.ok) {
-      console.error("Something went wrong");
+      resultatDiv.textContent = "";
+      document.getElementById("error").textContent = "Something went wrong";
       return;      
     }
 
@@ -49,7 +57,18 @@ async function submitForm() {
     resultatDiv.textContent = `${amount} ${devises[from].symbol} = ${result} ${devises[to].symbol}`;
     
   } catch (error) {
-    console.error(error);
+    resultatDiv.textContent = "";
+    document.getElementById("error").textContent = "Something went wrong";
     return;
   }
+}
+
+function switchDevises() {
+  const formData = new FormData(form);
+  const { from, to } = Object.fromEntries(formData.entries())
+
+  form.querySelector('select[name="from"]').value = to;
+  form.querySelector('select[name="to"]').value = from;
+
+  submitForm();
 }
